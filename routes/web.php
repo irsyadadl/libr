60ce95e5a93069d3f7c32d724ec9b4cdac2371a7
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-07-27 06:16:14 +0700

    update to vite 3

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/dashboard', DashboardController::class)->name('dashboard');
